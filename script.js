33c5e7dcdaad3d6147d22868f1206263a8af0f9b
Ori <ori100no@gmail.com>
2023-05-11 10:42:38 +0300

    added an help menu and a functionaly to categorize tasks by importance

--- script.js
+++ script.js
@@ -1,62 +1,67 @@
 // Retrieve DOM elements
 const taskForm = document.getElementById('task-form');
 const taskInput = document.getElementById('task-input');
+const categorySelect = document.getElementById('category-select');
 const taskList = document.getElementById('task-list');
 
-function createTaskItem(task) {
-    const li = document.createElement('li');
-    const taskText = document.createElement('span');
-    taskText.textContent = task;
-  
-    const deleteButton = document.createElement('button');
-    deleteButton.textContent = 'Delete';
-    deleteButton.classList.add('delete-button');
-  
-    deleteButton.addEventListener('click', function (event) {
-      event.stopPropagation(); // Stop event propagation
-  
-      // Remove the parent list item
-      li.remove();
-    });
-  
-    const upButton = document.createElement('button');
-    upButton.textContent = 'Up';
-    upButton.classList.add('move-button', 'up-button');
-  
-    const downButton = document.createElement('button');
-    downButton.textContent = 'Down';
-    downButton.classList.add('move-button', 'down-button');
-  
-    // Add event listeners to up and down buttons
-    upButton.addEventListener('click', function () {
-      moveTaskUp(li);
-    });
-  
-    downButton.addEventListener('click', function () {
-      moveTaskDown(li);
-    });
-  
-    li.appendChild(taskText);
-    li.appendChild(upButton);
-    li.appendChild(downButton);
-    li.appendChild(deleteButton);
-    taskList.appendChild(li);
+function createTaskItem(task, category) {
+  const li = document.createElement('li');
+  const taskText = document.createElement('span');
+  taskText.textContent = task;
+
+  const deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.classList.add('delete-button');
+
+  deleteButton.addEventListener('click', function (event) {
+    event.stopPropagation(); // Stop event propagation
+    li.remove(); // Remove the parent list item
+  });
+
+  const upButton = document.createElement('button');
+  upButton.textContent = 'Up';
+  upButton.classList.add('move-button', 'up-button');
+
+  const downButton = document.createElement('button');
+  downButton.textContent = 'Down';
+  downButton.classList.add('move-button', 'down-button');
+
+  upButton.addEventListener('click', function () {
+    moveTaskUp(li);
+  });
+
+  downButton.addEventListener('click', function () {
+    moveTaskDown(li);
+  });
+
+  li.appendChild(taskText);
+  li.appendChild(upButton);
+  li.appendChild(downButton);
+  li.appendChild(deleteButton);
+
+  // Apply category styles
+  if (category === 'urgent') {
+    li.classList.add('urgent-task');
+  } else if (category === 'non-urgent') {
+    li.classList.add('non-urgent-task');
   }
+
+  taskList.appendChild(li);
+}
+
   
-
-
 // Event listener for form submission
 taskForm.addEventListener('submit', function (e) {
   e.preventDefault(); // Prevent form submission
 
   const task = taskInput.value.trim(); // Get task value and remove leading/trailing spaces
+  const category = categorySelect.value; // Get selected category
 
   if (task !== '') {
-    createTaskItem(task); // Create new task item
+    createTaskItem(task, category); // Create new task item with category
     taskInput.value = ''; // Clear task input field
   }
 });
-
 
 // Function to move task item up
 function moveTaskUp(taskItem) {
@@ -89,6 +94,7 @@ taskList.addEventListener('click', function (e) {
       moveTaskDown(taskItem);
     }
   });
+  
 
 
 
